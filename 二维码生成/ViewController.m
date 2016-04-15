0ad362ab043993478d7ff8c2f2bc1082300a1c9e
liujie <liujie@lingtouniao.com>
2016-04-15 17:44:19 +0800

    add readme images

--- 二维码生成/ViewController.m
+++ 二维码生成/ViewController.m
@@ -24,12 +24,12 @@
 
 - (IBAction)creatQRCode:(UIButton *)sender {
     
-//    UIImage * qrCodeImage = [UIImage imageOfQRFromString:@"www.baidu.com" codeSize:CGRectGetWidth(self.QRCodeImageView.frame)];
+    UIImage * qrCodeImage = [UIImage imageOfQRFromString:@"www.baidu.com" codeSize:CGRectGetWidth(self.QRCodeImageView.frame)];
 //    UIImage * qrCodeImage = [UIImage imageOfQRFromString:@"www.baidu.com" codeSize:CGRectGetWidth(self.QRCodeImageView.frame) insertImage:[UIImage imageNamed:@"icon160*160"]];
-//    UIImage * qrCodeImage = [UIImage imageOfQRFromString:@"www.baidu.com" codeSize:CGRectGetWidth(self.QRCodeImageView.frame) insertImage:[UIImage imageNamed:@"icon160*160"] roundRadius:5];
+//    UIImage * qrCodeImage = [UIImage imageOfQRFromString:@"www.baidu.com" codeSize:CGRectGetWidth(self.QRCodeImageView.frame) insertImage:[UIImage imageNamed:@"icon160*160"] roundRadius:20];
 //    UIImage * qrCodeImage = [UIImage imageOfQRFromString:@"www.baidu.com" codeSize:CGRectGetWidth(self.QRCodeImageView.frame) red:250 green:110 blue:20];
 //    UIImage * qrCodeImage = [UIImage imageOfQRFromString:@"www.baidu.com" codeSize:CGRectGetWidth(self.QRCodeImageView.frame) red:250 green:110 blue:20 insertImage:[UIImage imageNamed:@"icon160*160"]];
-    UIImage * qrCodeImage = [UIImage imageOfQRFromString:@"www.baidu.com" codeSize:CGRectGetWidth(self.QRCodeImageView.frame) red:250 green:110 blue:20 insertImage:[UIImage imageNamed:@"icon160*160"] roundRadius:20];
+//    UIImage * qrCodeImage = [UIImage imageOfQRFromString:@"www.baidu.com" codeSize:CGRectGetWidth(self.QRCodeImageView.frame) red:250 green:110 blue:20 insertImage:[UIImage imageNamed:@"icon160*160"] roundRadius:20];
     self.QRCodeImageView.image = qrCodeImage;
 }
 
